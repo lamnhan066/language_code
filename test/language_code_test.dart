da0de51304488a214e84d24d3e03d360a520666f
VNNiz <contact@niz.vn>
2023-04-22 09:32:00 +0700

    Add test for `LanguageCodes.testCode`

--- test/language_code_test.dart
+++ test/language_code_test.dart
@@ -6,4 +6,12 @@ void main() {
     expect(LanguageCode.rawCode, isNotEmpty);
     expect(LanguageCode.locale.languageCode, equals(LanguageCode.rawCode));
   });
+
+  test('set test LanguageCodes', () {
+    LanguageCode.setTestCode(LanguageCodes.cu);
+    const code = LanguageCodes.cu;
+    expect(LanguageCode.rawCode, code.code);
+    expect(LanguageCode.locale, equals(code.locale));
+    expect(LanguageCode.code, equals(code));
+  });
 }
