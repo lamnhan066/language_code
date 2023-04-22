ff3d28d215714d1e8228937392d0b7cd2349c9c9
VNNiz <contact@niz.vn>
2023-04-22 09:31:53 +0700

    Add `LanguageCodes.testCode`

--- lib/src/language_code.dart
+++ lib/src/language_code.dart
@@ -9,12 +9,22 @@ import 'language_code_stub.dart'
 class LanguageCode {
   LanguageCode._();
 
+  static LanguageCodes? _testCode;
+
+  /// Using this method if you want to set a specific [LanguageCodes] for testing.
+  /// Just set it to `null` if you want to use the real value.
+  static void setTestCode([LanguageCodes? testCode]) {
+    _testCode = testCode;
+  }
+
   /// Get current language code of the device. This [rawCode] can contain the
   /// '-' or '_' character, you can use [locale] or [code] insteads.
-  static String get rawCode => languaeCodeImpl;
+  static String get rawCode => _testCode?.code ?? languaeCodeImpl;
 
   /// Get current language locale of the device
   static Locale get locale {
+    if (_testCode != null) return _testCode!.locale;
+
     final String token = rawCode.contains('-') ? '-' : '_';
     List localeList = rawCode.split(token);
 
@@ -26,5 +36,6 @@ class LanguageCode {
   }
 
   /// Get the language code as [LanguageCodes].
-  static LanguageCodes get code => LanguageCodes.fromLocale(locale);
+  static LanguageCodes get code =>
+      _testCode ?? LanguageCodes.fromLocale(locale);
 }
